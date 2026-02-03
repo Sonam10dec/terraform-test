b7164ee7d3a3d7ffbad7c370d1f1b79f8a7a2193
Sonam Khera <sonam.khera@ifi.tech>
2026-02-03 15:17:14 +0530

    Initial Terraform code for Policy as Code lab

--- main.tf
+++ main.tf
@@ -54,7 +54,6 @@ resource "azurerm_network_interface" "example" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
   }
 
   tags = {
